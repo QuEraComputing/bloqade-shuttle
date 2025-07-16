afd10d38010d58b7b7ad13d18e3681404a93c8cf
Phillip Weinberg <weinbe58@gmail.com>
2025-07-16 16:32:48 -0400

    Adding spec interpreter for path dialect (#30)
    * adding spec interpreter for path dialect
    
    * forcing tracer to always use tweezer dialect
    
    * moving import into function
    
    * removing post_init
    
    * adding cache to reduce overhead on import

--- src/bloqade/shuttle/dialects/path/spec_interp.py
+++ src/bloqade/shuttle/dialects/path/spec_interp.py
@@ -1,23 +1,21 @@
 from kirin.interp import (
     Frame,
-    Interpreter,
     InterpreterError,
     MethodTable,
     impl,
 )
 
+from bloqade.shuttle.arch import ArchSpecInterpreter
 from bloqade.shuttle.codegen import TraceInterpreter, reverse_path
 from bloqade.shuttle.dialects import schedule
 from bloqade.shuttle.dialects.path import dialect, stmts, types
 
 
-@dialect.register
-class PathInterpreter(MethodTable):
+@dialect.register(key="spec.interp")
+class SpecPathInterpreter(MethodTable):
 
     @impl(stmts.Gen)
-    def gen(self, interp: Interpreter, frame: Frame, stmt: stmts.Gen):
-        if stmt.arch_spec is None:
-            raise InterpreterError("Missing architecture specification")
+    def gen(self, interp: ArchSpecInterpreter, frame: Frame, stmt: stmts.Gen):
 
         device_task = frame.get(stmt.device_task)
         if isinstance(device_task, schedule.DeviceFunction):
@@ -31,7 +29,9 @@ class PathInterpreter(MethodTable):
         inputs = frame.get_values(stmt.inputs)
         kwargs = stmt.kwargs
         args = interp.permute_values(device_task.move_fn.arg_names, inputs, kwargs)
-        path = TraceInterpreter(stmt.arch_spec).run_trace(device_task.move_fn, args, {})
+        path = TraceInterpreter(interp.arch_spec).run_trace(
+            device_task.move_fn, args, {}
+        )
 
         if reverse:
             path = reverse_path(path)
