a474bac87b9d7fdd263433bd2cc1681056a1a2c6
Phillip Weinberg <weinbe58@gmail.com>
2025-08-14 14:24:11 -0400

    Adding test

---
+++ test/unit/test_arch.py
@@ -0,0 +1,12 @@
+from bloqade.geometry.dialects.grid import Grid
+
+from bloqade.shuttle.arch import Layout
+
+
+def test_layout():
+
+    layout = Layout({"test": Grid.from_positions(range(16), range(16))}, {"test"})
+
+    assert hash(layout) == hash(
+        (frozenset(layout.static_traps.items()), frozenset(layout.fillable))
+    )
