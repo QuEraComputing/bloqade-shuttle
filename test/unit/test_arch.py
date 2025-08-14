bb1f0c3c6d9da16393a52223d3958fe26d073a86
Phillip Weinberg <weinbe58@gmail.com>
2025-08-14 14:31:23 -0400

    adding some basic tests

--- test/unit/test_arch.py
+++ test/unit/test_arch.py
@@ -10,3 +10,7 @@ def test_layout():
     assert hash(layout) == hash(
         (frozenset(layout.static_traps.items()), frozenset(layout.fillable))
     )
+    assert layout == Layout(
+        {"test": Grid.from_positions(range(16), range(16))}, {"test"}
+    )
+    assert layout != 1
