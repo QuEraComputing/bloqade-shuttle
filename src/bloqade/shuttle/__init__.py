457e4d984e30c530d552b4c3336e279360790746
Phillip Weinberg <weinbe58@gmail.com>
2025-08-15 09:35:28 -0400

    adding imports

--- src/bloqade/shuttle/__init__.py
+++ src/bloqade/shuttle/__init__.py
@@ -1,13 +1,12 @@
-from . import arch as arch, passes as passes, stdlib as stdlib
-from .dialects.action import _interface as action
-from .dialects.atom import _interface as atom
-from .dialects.filled import _interface as filled
-from .dialects.gate import _interface as gate
-from .dialects.init import _interface as init
-from .dialects.measure import _interface as measure
-from .dialects.schedule import _interface as schedule
-from .dialects.spec import _interface as spec
-from .prelude import kernel as kernel, move as move, tweezer as tweezer
+from bloqade.shuttle.dialects.action import _interface as action
+from bloqade.shuttle.dialects.atom import _interface as atom
+from bloqade.shuttle.dialects.filled import _interface as filled
+from bloqade.shuttle.dialects.gate import _interface as gate
+from bloqade.shuttle.dialects.init import _interface as init
+from bloqade.shuttle.dialects.measure import _interface as measure
+from bloqade.shuttle.dialects.schedule import _interface as schedule
+from bloqade.shuttle.dialects.spec import _interface as spec
+from bloqade.shuttle.prelude import kernel as kernel, move as move, tweezer as tweezer
 
 __all__ = [
     "action",
