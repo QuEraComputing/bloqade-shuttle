dbe2ff5e8a8be14f2f9d9e851183d24d17cac0ab
WanHsuanLin <b06901054@ntu.edu.tw>
2025-08-17 14:25:18 -0700

    add draft

---
+++ scratch/lower_zair.py
@@ -0,0 +1,122 @@
+from dataclasses import dataclass, field
+import math
+from typing import Any, Sequence
+from kirin import ir
+from kirin.dialects.ilist import IList
+from kirin.dialects import py, func
+from bloqade.shuttle import gate, init, spec
+from bloqade.geometry import grid
+from bloqade.shuttle.dialects import gate, filled
+
+
+def _simple_region() -> ir.Region:
+    return ir.Region(ir.Block())
+
+
+@dataclass
+class ShuttleBuilder:
+    move_kernel: ir.Method[[IList[tuple[int, int]], IList[tuple[int, int]]], None]
+    num_qubits: int
+
+    body: ir.Region = field(default_factory=_simple_region, init=False)
+    grid_mapping: dict[int, ir.SSAValue] = field(default_factory=dict)
+
+    def push_stmt(self, stmt: ir.Statement):
+        self.body.blocks[0].stmts.append(stmt)
+        return stmt
+
+    def push_constant(self, value: Any) -> ir.SSAValue:
+        const_stmt = py.Constant(value)
+        return self.push_stmt(const_stmt).expect_one_result()
+
+    def construct_grid(
+        self,
+        grid_id: int,
+        offset: tuple[int, int],
+        x_spacing: list[int],
+        y_spacing: list[int],
+        dim: tuple[int, int],
+    ):
+        x_init = offset[0]
+        y_init = offset[1]
+
+        grid_ref = self.push_constant(
+            grid.Grid(
+                x_spacing=tuple(x_spacing), y_spacing=tuple(y_spacing), x_init=x_init, y_init=y_init
+            )
+        )
+
+        self.grid_mapping[grid_id] = grid_ref
+
+        return grid_ref
+
+    def insert_move(
+        self,
+        srcs: Sequence[tuple[int, int, int, int]],
+        dsts: Sequence[tuple[int, int, int, int]],
+    ):
+        # ignoring zone mapping,
+        sorted_srcs = sorted(srcs, key=lambda x: x[0])
+        sorted_dsts = sorted(dsts, key=lambda x: x[0])
+
+        x_src = [src[2] for src in sorted_srcs]
+        y_src = [src[3] for src in sorted_srcs]
+        x_dst = [dst[2] for dst in sorted_dsts]
+        y_dst = [dst[3] for dst in sorted_dsts]
+
+        x_src_ref = self.push_constant(x_src)
+        y_src_ref = self.push_constant(y_src)
+        x_dst_ref = self.push_constant(x_dst)
+        y_dst_ref = self.push_constant(y_dst)
+
+        self.push_stmt(
+            func.Invoke(
+                inputs=(x_src_ref, y_src_ref, x_dst_ref, y_dst_ref),
+                callee=self.move_kernel,
+                kwargs=(),
+            )
+        )
+    
+    def single_zone_move_impl(
+        src_x: IList[int, Any],
+        src_y: IList[int, Any],
+        dst_x: IList[int, Any],
+        dst_y: IList[int, Any],
+    ):
+        ...
+
+    def entangle(self, grid_id: int):
+        self.push_stmt(gate.TopHatCZ(self.grid_mapping[grid_id]))
+
+    def r_gate(
+        self,
+        axis_angle: float,
+        rotation_angle: float,
+        locs: Sequence[tuple[int, int, int, int]],
+    ):
+        filled_locs = {}
+
+        for _, grid_id, x, y in locs:
+            filled_locs.setdefault(grid_id, []).append((x, y))
+
+        filled_loc_refs: dict[int, ir.SSAValue] = {}
+
+        for grid_id, locs in filled_locs.items():
+            locs_ref = self.push_constant(locs)
+            filled_loc_refs[grid_id] = self.push_stmt(
+                filled.Fill(self.grid_mapping[grid_id], locs_ref)
+            ).expect_one_result()
+
+        axis_angle_ref = self.push_constant(axis_angle / (2 * math.pi))
+        rotation_angle_ref = self.push_constant(rotation_angle / (2 * math.pi))
+
+        for filled_ref in filled_loc_refs.values():
+            self.push_stmt(gate.LocalR(axis_angle_ref, rotation_angle_ref, filled_ref))
+
+    def lower_h(self, locs: Sequence[tuple[int, int, int, int]]):
+        assert len(locs) == self.num_qubits, "H gate must be applied to all qubits"
+        self.push_stmt(gate.GlobalRz(0.25))
+        self.push_stmt(gate.GlobalR(0, 0.5))
+        self.push_stmt(gate.GlobalRz(0.25))
+
+
---
+++ scratch/qcrank.py
@@ -0,0 +1,123 @@
+from typing import Any, Literal, TypeVar
+
+from bloqade.geometry.dialects import grid
+from kirin.dialects import ilist
+
+from bloqade.shuttle import action, gate, init, measure, schedule, spec
+from bloqade.shuttle.prelude import move, tweezer
+from bloqade.shuttle.visualizer import MatplotlibRenderer, PathVisualizer
+from lower_zair import ShuttleBuilder
+import json
+import numpy as np
+
+@move
+def lower_zair(shuttle_builder, insts: list):
+    for inst in insts:
+        if inst["type"] == "1qGate":
+            if inst["unitary"] == "ry":
+                for loc in inst["locs"]:
+                    rotation_angle = np.random.random()
+                    shuttle_builder.r_gate(0, rotation_angle, [loc])
+                pass
+            elif inst["unitary"] == "h":
+                pass
+                shuttle_builder.lower_h(inst["locs"])
+            else:
+                raise NotImplementedError
+
+        elif inst["type"] == "rydberg":
+            shuttle_builder.entangle(inst["zone_id"])
+        elif inst["type"] == "rearrangeJob":
+            shuttle_builder.insert_move(inst["begin_locs", inst["end_locs"]])
+        else:
+            raise NotImplementedError
+
+def run_qcrank(filename: str):
+    with open(filename, 'r') as f:
+        compiled_qcrank = json.load(f)
+    
+    arch_filename = compiled_qcrank["architecture_spec_path"]
+
+    with open(arch_filename, 'r') as f:
+        architecture_spec = json.load(f)
+
+    # set architecture
+    # assume single entagnlement zone
+    entanglement_zone_spec =  architecture_spec["entanglement_zones"]
+    slms = entanglement_zone_spec["slms"]
+    assert len(slms) == 2
+    slm0 = slms[0]
+    slm1 = slms[1]
+    assert slm0["r"] == slm1["r"]
+    assert slm0["c"] == slm1["c"]
+    assert slm0["site_seperation"] == slm1["site_seperation"]
+    put_vertical = True
+    if slm0["location"][1] == slm1["location"][1]:
+        put_vertical = False
+    if put_vertical:
+        dis_trap = abs(slm0["location"][1] - slm1["location"][1])
+        dis_site = slm0["site_seperation"][1] - dis_trap
+        x_spacing = [slm0["site_seperation"][0] * (slm0["r"] - 1)]
+        y_spacing = []
+        for _ in slm0["c"]:
+            y_spacing.append(dis_trap)
+            y_spacing.append(dis_site)
+    else:
+        dis_trap = abs(slm0["location"][0] - slm1["location"][0])
+        dis_site = slm0["site_seperation"][0] - dis_trap
+        y_spacing = [slm0["site_seperation"][1] * (slm0["r"] - 1)]
+        x_spacing = []
+        for _ in slm0["c"]:
+            x_spacing.append(dis_trap)
+            x_spacing.append(dis_site)
+        x_spacing = x_spacing
+    
+    inst_init = compiled_qcrank["instructions"][0]
+    init_quibt_location = inst_init["init_locs"]
+    
+    shuttle_builder = ShuttleBuilder(num_qubits = len(init_quibt_location))
+    shuttle_builder.construct_grid(entanglement_zone_spec['zone_id'],
+                                   entanglement_zone_spec["offset"],
+                                   x_spacing,
+                                   y_spacing,
+                                   entanglement_zone_spec["dimension"])
+
+    spec_value = spec.ArchSpec(
+        layout=spec.Layout(
+            static_traps={
+                "mem": shuttle_builder.grid_mapping[0],
+            },
+            fillable=set(["mem"]),
+        )
+    )
+
+    grid_init_quibt_location = []
+    for loc in init_quibt_location:
+        x = loc[2]
+        y = loc[3]
+        if put_vertical:
+            y *= 2
+        else:
+            x *= 2
+        grid_init_quibt_location.append((x,y))
+
+    @move
+    def main():
+        init.fill(grid_init_quibt_location)
+        insts = compiled_qcrank["instructions"][1:]
+        lower_zair(shuttle_builder, insts)
+        
+        return measure.measure(shuttle_builder.grid_mapping[0])
+
+    return main, spec_value
+
+
+def run_plotter(filename: str):
+    main, spec_value = run_qcrank(filename)
+    renderer = MatplotlibRenderer()
+    PathVisualizer(main.dialects, renderer=renderer, arch_spec=spec_value).run(main, ())
+
+
+if __name__ == "__main__":
+    filename = "scratch/qcr_4a8d_quera_circ_code.json"
+    run_plotter(filename)
