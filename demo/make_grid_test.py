c8da3b42e73b03269be72a30a936bded83978b8e
tcochran-quera <tcochran@quera.com>
2025-09-17 17:37:06 -0400

    Add reservoir sites and inter-zone moves.

--- demo/make_grid_test.py
+++ demo/make_grid_test.py
@@ -18,6 +18,25 @@ NumX = TypeVar('NumX', bound=int)
 NumY = TypeVar('NumY', bound=int)
 
 @tweezer
+def interzone_move(
+    storage_grid: grid.Grid[Any, Literal[1]],
+    entangling_grid: grid.Grid[Any, Literal[1]],
+):
+
+    start_x = grid.get_xpos(storage_grid)
+    start_y = grid.get_ypos(storage_grid)
+
+    end_x = grid.get_xpos(entangling_grid)
+    end_y = grid.get_ypos(entangling_grid)
+
+    pos_1 = grid.from_positions(start_x, start_y)
+    pos_2 = grid.from_positions(end_x, end_y)
+
+    action.set_loc(pos_1)
+    action.turn_on(action.ALL, [0])
+    action.move(pos_2)
+
+@tweezer
 def entangle_move_ltor(
     controls: grid.Grid[Any, Literal[1]],
     targets: grid.Grid[Any, Literal[1]],
@@ -56,6 +75,20 @@ def entangle_move_rtor(
     action.move(pos_2)
 
 @move
+def run_interzone_move(
+    storage_grid: grid.Grid[Any, Literal[1]],
+    entangling_grid: grid.Grid[Any, Literal[1]],
+):
+    xtones = ilist.range(len(storage_grid.x_positions))
+    ytones = ilist.range(len(storage_grid.y_positions))
+
+    dtask = schedule.device_fn(interzone_move, xtones, ytones)
+    rev_dtask = schedule.reverse(dtask)
+
+    dtask(storage_grid, entangling_grid)
+    rev_dtask(storage_grid, entangling_grid)
+
+@move
 def run_entangle_move_ltor(
     controls: grid.Grid[Any, Literal[1]],
     targets: grid.Grid[Any, Literal[1]],
@@ -87,29 +120,98 @@ def run_entangle_move_rtor(
 
 def generate_moves():
 
-    num_x = 14
+    num_x = 17
     num_y = 5
-    spacing = 10.0
-    gate_spacing = 2.0
+    column_spacing = 8.0
+    row_spacing = 10.0
+    gate_spacing = 2.0 #space between atoms for CZ gate.
 
-    x_spacing = sum(repeat((gate_spacing, spacing), num_x - 1), ()) + (gate_spacing,)
-    y_spacing = tuple(repeat(spacing, num_y - 1))
-    all_traps = grid.Grid(x_spacing, y_spacing, 0.0, 0.0)
+    x_spacing = sum(repeat((gate_spacing, column_spacing), num_x - 1), ()) + (gate_spacing,)
+    y_spacing = tuple(repeat(row_spacing, num_y - 1))
+    all_entanglinz_zone_traps = grid.Grid(x_spacing, y_spacing, -81.0, -20.0) # offset to center the grid such that 0,0 is the middle of the AOD.
     left_trap_x_indices = ilist.IList(range(0, num_x * 2, 2))
     right_trap_x_indices = ilist.IList(range(1, num_x * 2, 2))
     all_y_indices = ilist.IList(range(num_y))
-    left_traps = all_traps.get_view(left_trap_x_indices, all_y_indices)
-    right_traps = all_traps.get_view(right_trap_x_indices, all_y_indices)
+    left_traps = all_entanglinz_zone_traps.get_view(left_trap_x_indices, all_y_indices)
+    right_traps = all_entanglinz_zone_traps.get_view(right_trap_x_indices, all_y_indices)
+
+    reservoir_ally_width = 6.0
+    reservoir_column_spacing = column_spacing+gate_spacing-reservoir_ally_width
+    reservoir_row_spacing = 4.0
+    num_reservoir_rows = 19
+
+    reservoir_x_spacing = sum(repeat((reservoir_ally_width, reservoir_column_spacing), num_x - 1), ()) + (reservoir_ally_width,)
+    reservoir_y_spacing = tuple(repeat(reservoir_row_spacing, num_reservoir_rows - 1))
+
+    top_reservoir = grid.Grid(reservoir_x_spacing, reservoir_y_spacing, -81.0 - reservoir_ally_width, row_spacing * 3)
+    bottom_reservoir = grid.Grid(reservoir_x_spacing, reservoir_y_spacing, -81.0 - reservoir_ally_width, -3.0 * row_spacing - reservoir_row_spacing * (num_reservoir_rows-1))
+
+    SL0_block_x_indices = ilist.IList(range(4,4+7*2,2))
+    SL0_block_y_indices = ilist.IList(range(8,8+5*2,2))
+    SL0_block_traps = top_reservoir.get_view(SL0_block_x_indices, SL0_block_y_indices)
+
+    SR0_block_x_indices = ilist.IList(range(5, 5 + 7 * 2, 2))
+    SR0_block_y_indices = ilist.IList(range(8, 8 + 5 * 2, 2))
+    SR0_block_traps = top_reservoir.get_view(SR0_block_x_indices, SR0_block_y_indices)
+
+    SL1_block_x_indices = ilist.IList(range(4 + 7*2, 4 + 7*2 + 7 * 2, 2))
+    SL1_block_y_indices = ilist.IList(range(8, 8 + 5 * 2, 2))
+    SL1_block_traps = top_reservoir.get_view(SL1_block_x_indices, SL1_block_y_indices)
+
+    SR1_block_x_indices = ilist.IList(range(5 + 7*2, 5 + 7*2 + 7 * 2, 2))
+    SR1_block_y_indices = ilist.IList(range(8, 8 + 5 * 2, 2))
+    SR1_block_traps = top_reservoir.get_view(SR1_block_x_indices, SR1_block_y_indices)
+
+    ML0_block_x_indices = ilist.IList(range(4, 4 + 7 * 2, 2))
+    ML0_block_y_indices = ilist.IList(range(2, 2 + 5 * 2, 2))
+    ML0_block_traps = bottom_reservoir.get_view(ML0_block_x_indices, ML0_block_y_indices)
+
+    MR0_block_x_indices = ilist.IList(range(5, 5 + 7 * 2, 2))
+    MR0_block_y_indices = ilist.IList(range(2, 2 + 5 * 2, 2))
+    MR0_block_traps = bottom_reservoir.get_view(MR0_block_x_indices, MR0_block_y_indices)
+
+    ML1_block_x_indices = ilist.IList(range(4 + 7 * 2, 4 + 7 * 2 + 7 * 2, 2))
+    ML1_block_y_indices = ilist.IList(range(2, 2 + 5 * 2, 2))
+    ML1_block_traps = bottom_reservoir.get_view(ML1_block_x_indices, ML1_block_y_indices)
+
+    MR1_block_x_indices = ilist.IList(range(5 + 7 * 2, 5 + 7 * 2 + 7 * 2, 2))
+    MR1_block_y_indices = ilist.IList(range(2, 2 + 5 * 2, 2))
+    MR1_block_traps = bottom_reservoir.get_view(MR1_block_x_indices, MR1_block_y_indices)
+
+    G0_block_x_indices = ilist.IList(range(2, 2+7))
+    GL0_block_traps = left_traps.get_view(G0_block_x_indices, all_y_indices)
+    GR0_block_traps = right_traps.get_view(G0_block_x_indices, all_y_indices)
+
+    G1_block_x_indices = ilist.IList(range(2 + 7, 2 + 7 + 7))
+    GL1_block_traps = left_traps.get_view(G1_block_x_indices, all_y_indices)
+    GR1_block_traps = right_traps.get_view(G1_block_x_indices, all_y_indices)
 
     static_traps = {
-        "traps": all_traps,
+        "traps": all_entanglinz_zone_traps,
         "left_traps": left_traps,
         "right_traps": right_traps,
+        "top_reservoir": top_reservoir,
+        "bottom_reservoir": bottom_reservoir,
+        "SL0_block": SL0_block_traps,
+        "SR0_block": SR0_block_traps,
+        "SL1_block": SL1_block_traps,
+        "SR1_block": SR1_block_traps,
+        "ML0_block": ML0_block_traps,
+        "MR0_block": MR0_block_traps,
+        "ML1_block": ML1_block_traps,
+        "MR1_block": MR1_block_traps,
+        "GL0_block": GL0_block_traps,
+        "GR0_block": GR0_block_traps,
+        "GL1_block": GL1_block_traps,
+        "GR1_block": GR1_block_traps,
     }
+    
+    logical_rows = 5
+    logical_cols = 2
 
     home_x_indices = {
-        logical_q_idx: ilist.IList([(q_idx % 14) for q_idx in range(logical_q_idx * 7, logical_q_idx * 7 + 7)]) for
-        logical_q_idx in range(int(num_x * num_y / 7))
+        logical_q_idx: ilist.IList([(q_idx % 14)+2 for q_idx in range(logical_q_idx * 7, logical_q_idx * 7 + 7)]) for
+        logical_q_idx in range(int(logical_cols * logical_rows))
     }
 
     home_y_indices = {
@@ -167,7 +269,7 @@ def generate_moves():
 
     layout = spec.Layout(
         static_traps=static_traps,
-        fillable=set(["left_traps"]),
+        fillable=set(["GL0_block","GL1_block","SL0_block","SR0_block","SL1_block","SR1_block", "ML0_block","MR0_block","ML1_block","MR1_block"]),
         has_cz=set(["traps"]),
         has_local=set(["left_traps"]),
     )
@@ -258,6 +360,70 @@ def generate_moves():
         controls = spec.get_static_trap(zone_id="controls_18")
         targets = spec.get_static_trap(zone_id="targets_18")
         run_entangle_move_ltor(controls, targets)
+
+        controls = spec.get_static_trap(zone_id="SL0_block")
+        targets = spec.get_static_trap(zone_id="GL0_block")
+        run_interzone_move(controls, targets)
+
+        controls = spec.get_static_trap(zone_id="SR0_block")
+        targets = spec.get_static_trap(zone_id="GL0_block")
+        run_interzone_move(controls, targets)
+
+        controls = spec.get_static_trap(zone_id="SL1_block")
+        targets = spec.get_static_trap(zone_id="GL0_block")
+        run_interzone_move(controls, targets)
+
+        controls = spec.get_static_trap(zone_id="SR1_block")
+        targets = spec.get_static_trap(zone_id="GL0_block")
+        run_interzone_move(controls, targets)
+
+        controls = spec.get_static_trap(zone_id="SL0_block")
+        targets = spec.get_static_trap(zone_id="GL1_block")
+        run_interzone_move(controls, targets)
+
+        controls = spec.get_static_trap(zone_id="SR0_block")
+        targets = spec.get_static_trap(zone_id="GL1_block")
+        run_interzone_move(controls, targets)
+
+        controls = spec.get_static_trap(zone_id="SL1_block")
+        targets = spec.get_static_trap(zone_id="GL1_block")
+        run_interzone_move(controls, targets)
+
+        controls = spec.get_static_trap(zone_id="SR1_block")
+        targets = spec.get_static_trap(zone_id="GL1_block")
+        run_interzone_move(controls, targets)
+
+        controls = spec.get_static_trap(zone_id="ML0_block")
+        targets = spec.get_static_trap(zone_id="GL0_block")
+        run_interzone_move(controls, targets)
+
+        controls = spec.get_static_trap(zone_id="MR0_block")
+        targets = spec.get_static_trap(zone_id="GL0_block")
+        run_interzone_move(controls, targets)
+
+        controls = spec.get_static_trap(zone_id="ML1_block")
+        targets = spec.get_static_trap(zone_id="GL0_block")
+        run_interzone_move(controls, targets)
+
+        controls = spec.get_static_trap(zone_id="MR1_block")
+        targets = spec.get_static_trap(zone_id="GL0_block")
+        run_interzone_move(controls, targets)
+
+        controls = spec.get_static_trap(zone_id="ML0_block")
+        targets = spec.get_static_trap(zone_id="GL1_block")
+        run_interzone_move(controls, targets)
+
+        controls = spec.get_static_trap(zone_id="MR0_block")
+        targets = spec.get_static_trap(zone_id="GL1_block")
+        run_interzone_move(controls, targets)
+
+        controls = spec.get_static_trap(zone_id="ML1_block")
+        targets = spec.get_static_trap(zone_id="GL1_block")
+        run_interzone_move(controls, targets)
+
+        controls = spec.get_static_trap(zone_id="MR1_block")
+        targets = spec.get_static_trap(zone_id="GL1_block")
+        run_interzone_move(controls, targets)
 
         return measure.measure((left_traps,))
 
