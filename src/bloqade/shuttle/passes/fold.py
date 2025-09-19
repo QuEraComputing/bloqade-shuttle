b28bc7b00003801318ee68ffe3586756b3dac0ec
Phillip Weinberg <weinbe58@gmail.com>
2025-09-19 11:41:55 -0400

    Refactoring unroll pass

--- src/bloqade/shuttle/passes/fold.py
+++ src/bloqade/shuttle/passes/fold.py
@@ -4,6 +4,7 @@ from kirin import ir
 from kirin.dialects import ilist, scf
 from kirin.ir.method import Method
 from kirin.passes import HintConst, Pass, TypeInfer
+from kirin.passes.aggressive import UnrollScf
 from kirin.rewrite import (
     Call2Invoke,
     CFGCompactify,
@@ -21,48 +22,52 @@ from kirin.rewrite.cse import CommonSubexpressionElimination
 
 
 @dataclass
-class AggressiveUnroll(Pass):
-    """Fold pass to fold control flow"""
-
-    constprop: HintConst = field(init=False)
-    typeinfer: TypeInfer = field(init=False)
-
+class Fold(Pass):
+    hint_const: HintConst = field(init=False)
+    
+    
     def __post_init__(self):
-        self.const_hint = HintConst(self.dialects, no_raise=self.no_raise)
-        self.typeinfer = TypeInfer(self.dialects, no_raise=self.no_raise)
-
+        self.hint_const = HintConst(self.dialects, no_raise=self.no_raise)
+        
+        
     def unsafe_run(self, mt: Method) -> RewriteResult:
         result = RewriteResult()
-        result = self.const_hint.unsafe_run(mt).join(result)
+        result = self.hint_const.unsafe_run(mt).join(result)
         rule = Chain(
             ConstantFold(),
             Call2Invoke(),
             InlineGetField(),
             InlineGetItem(),
             ilist.rewrite.InlineGetItem(),
+            ilist.rewrite.HintLen(),
             DeadCodeElimination(),
             CommonSubexpressionElimination(),
         )
-        result = Fixpoint(Walk(rule)).rewrite(mt.code).join(result)
-        result = (
-            Walk(
-                Chain(
-                    scf.unroll.PickIfElse(),
-                    scf.unroll.ForLoop(),
-                    scf.trim.UnusedYield(),
-                )
-            )
-            .rewrite(mt.code)
-            .join(result)
-        )
+        result =  Fixpoint(Walk(rule)).rewrite(mt.code).join(result)
+        
+        return result
 
-        self.typeinfer.unsafe_run(mt)
+@dataclass
+class AggressiveUnroll(Pass):
+    """Fold pass to fold control flow"""
+
+    fold: Fold = field(init=False)
+    typeinfer: TypeInfer = field(init=False)
+
+    def __post_init__(self):
+        self.fold = Fold(self.dialects, no_raise=self.no_raise)
+        self.typeinfer = TypeInfer(self.dialects, no_raise=self.no_raise)
+
+    def unsafe_run(self, mt: Method) -> RewriteResult:
+        result = RewriteResult()
+        result = UnrollScf(self.dialects, no_raise=self.no_raise).unsafe_run(mt).join(result)
         result = (
             Walk(Chain(ilist.rewrite.ConstList2IList(), ilist.rewrite.Unroll()))
             .rewrite(mt.code)
             .join(result)
         )
-
+        result = self.typeinfer.unsafe_run(mt).join(result)
+        result = self.fold.unsafe_run(mt).join(result)
         result = Walk(Inline(self.inline_heuristic)).rewrite(mt.code).join(result)
         result = Walk(Fixpoint(CFGCompactify())).rewrite(mt.code).join(result)
         return result
@@ -73,10 +78,5 @@ class AggressiveUnroll(Pass):
         inside loops and if-else, only inline simple functions, i.e.
         functions with a single block
         """
-        if not isinstance(node.parent_stmt, (scf.For, scf.IfElse)):
-            return True  # always inline calls outside of loops and if-else
+        return not isinstance(node.parent_stmt, (scf.For, scf.IfElse))  # always inline calls outside of loops and if-else
 
-        if (trait := node.get_trait(ir.CallableStmtInterface)) is None:
-            return False  # not a callable, don't inline to be safe
-        region = trait.get_callable_region(node)
-        return len(region.blocks) == 1
