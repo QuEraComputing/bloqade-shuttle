b3ad7258c34ccdf0ed9e3a7b48bbc5059a95a270
Phillip Weinberg <weinbe58@gmail.com>
2025-10-01 10:59:21 -0400

    running linter and adding new stdlib

--- demo/logical_grid_and_moves.py
+++ demo/logical_grid_and_moves.py
@@ -1,25 +1,19 @@
-from itertools import repeat, chain
-from typing import Any, Literal, TypeVar, List
+from itertools import chain, repeat
+from typing import Any, List, Literal, TypeVar
 
-
+import matplotlib.pyplot as plt
 from bloqade.geometry.dialects import grid
+from bloqade.insight import kernel
+from bloqade.insight.interp import AnimationInterpreter
+from bloqade.insight.legacy.animate import FieldOfView, animate
+from bloqade.insight.upstream.shuttle import ShuttleToInsight
+from kirin import ir
 from kirin.dialects import ilist
 
 from bloqade.shuttle import action, gate, init, measure, schedule, spec
-
 from bloqade.shuttle.prelude import move, tweezer
-
 from bloqade.shuttle.stdlib.layouts.asserts import assert_sorted
-
 from bloqade.shuttle.visualizer import MatplotlibRenderer, PathVisualizer
-
-from bloqade.insight.upstream.shuttle import ShuttleToInsight
-from bloqade.insight.interp import AnimationInterpreter
-from bloqade.insight.legacy.animate import animate, FieldOfView
-from bloqade.insight import kernel
-from kirin import ir
-
-import matplotlib.pyplot as plt
 
 ROW_SEPARATION = 10.0
 COL_SEPARATION = 8.0
@@ -27,67 +21,14 @@ STORAGE_ALLY_WIDTH = 6.0
 GATE_SPACING = 2.0
 
 
-def animate_shuttle(
-    main: ir.Method,  # entry point of shuttle program
-    spec_value: spec.ArchSpec,  # architecture specification
-    display_fov: FieldOfView | None = None,
-    dilation_rate: float = 0.05,
-    fps: int = 30,
-    gate_display_dilation: float = 1.0,
-    fig_args={},
-    save_mpeg: bool = False,
-    filename: str = "vqpu_animation",
-    start_block: int = 0,
-    n_blocks: int | None = None,
-):
-
-    def get_fov(spec: spec.Layout):
-        x_min = float("inf")
-        x_max = float("-inf")
-        y_min = float("inf")
-        y_max = float("-inf")
-        for zone in chain(spec.static_traps.values(), spec.special_grid.values()):
-            x_bounds = zone.x_bounds()
-            y_bounds = zone.y_bounds()
-            if x_bounds[0] is not None:
-                x_min = min(x_min, x_bounds[0])
-            if x_bounds[1] is not None:
-                x_max = max(x_max, x_bounds[1])
-            if y_bounds[0] is not None:
-                y_min = min(y_min, y_bounds[0])
-            if y_bounds[1] is not None:
-                y_max = max(y_max, y_bounds[1])
-
-        return FieldOfView(x_min - 10, x_max + 10, y_min - 10, y_max + 10)
-
-    if display_fov is None:
-        display_fov = get_fov(spec_value.layout)
-
-    new_main = main.similar(kernel.union(move))
-    ShuttleToInsight(move.union(kernel), 1, spec_value)(new_main)
-    timeline = AnimationInterpreter(kernel).get_timeline(new_main, ())
-    return animate(
-        timeline,
-        display_fov=display_fov,
-        dilation_rate=dilation_rate,
-        fps=fps,
-        gate_display_dilation=gate_display_dilation,
-        fig_args=fig_args,
-        save_mpeg=save_mpeg,
-        filename=filename,
-        start_block=start_block,
-        n_blocks=n_blocks,
-    )
-
 # Define type variables for generic programming
-NumX = TypeVar('NumX', bound=int)
-NumY = TypeVar('NumY', bound=int)
+NumX = TypeVar("NumX", bound=int)
+NumY = TypeVar("NumY", bound=int)
+
 
 @tweezer
 def interzone_move(
-    storage_grid: grid.Grid[Any, Any],
-    entangling_grid: grid.Grid[Any, Any],
-    left:bool
+    storage_grid: grid.Grid[Any, Any], entangling_grid: grid.Grid[Any, Any], left: bool
 ):
 
     start_x = grid.get_xpos(storage_grid)
@@ -100,19 +41,19 @@ def interzone_move(
     pos_2 = grid.from_positions(end_x, end_y)
 
     if left:
-        shift_x = STORAGE_ALLY_WIDTH/2
+        shift_x = STORAGE_ALLY_WIDTH / 2
     else:
-        shift_x = -STORAGE_ALLY_WIDTH/2
+        shift_x = -STORAGE_ALLY_WIDTH / 2
 
     if start_y[0] > end_y[0]:
-        shift_y = ROW_SEPARATION/2
+        shift_y = ROW_SEPARATION / 2
     else:
-        shift_y = -ROW_SEPARATION/2
+        shift_y = -ROW_SEPARATION / 2
 
     mid_1 = grid.shift(pos_1, shift_x, 0)
-    mid_2 = grid.shift(pos_2, -STORAGE_ALLY_WIDTH/2, 0)
+    mid_2 = grid.shift(pos_2, -STORAGE_ALLY_WIDTH / 2, 0)
     mid_3 = grid.shift(pos_2, start_x[0] - end_x[0] + shift_x, shift_y)
-    mid_4 = grid.shift(pos_2,0,shift_y)
+    mid_4 = grid.shift(pos_2, 0, shift_y)
 
     action.set_loc(pos_1)
     action.turn_on(action.ALL, action.ALL)
@@ -125,6 +66,7 @@ def interzone_move(
         action.move(mid_4)
 
     action.move(pos_2)
+
 
 @tweezer
 def entangle_move_ltor(
@@ -143,16 +85,16 @@ def entangle_move_ltor(
 
     if start_y[0] < end_y[0]:
         # moving up
-        y_shift = ROW_SEPARATION/2
+        y_shift = ROW_SEPARATION / 2
     else:
         # moving down
-        y_shift = -ROW_SEPARATION/2
+        y_shift = -ROW_SEPARATION / 2
 
     if start_y == end_y:
         action.set_loc(pos_1)
         action.turn_on(action.ALL, action.ALL)
         action.move(pos_2)
-    elif start_y[0] - end_y[0] in [-ROW_SEPARATION,ROW_SEPARATION]:
+    elif start_y[0] - end_y[0] in [-ROW_SEPARATION, ROW_SEPARATION]:
         mid_1 = grid.shift(pos_1, 0, y_shift)
         mid_2 = grid.shift(pos_1, 2, y_shift)
 
@@ -163,8 +105,8 @@ def entangle_move_ltor(
         action.move(pos_2)
     else:
         mid_1 = grid.shift(pos_1, 0, y_shift)
-        mid_2 = grid.shift(pos_1, GATE_SPACING + COL_SEPARATION/2, y_shift)
-        mid_3 = grid.shift(pos_2, COL_SEPARATION/2, 0)
+        mid_2 = grid.shift(pos_1, GATE_SPACING + COL_SEPARATION / 2, y_shift)
+        mid_3 = grid.shift(pos_2, COL_SEPARATION / 2, 0)
 
         action.set_loc(pos_1)
         action.turn_on(action.ALL, action.ALL)
@@ -172,6 +114,7 @@ def entangle_move_ltor(
         action.move(mid_2)
         action.move(mid_3)
         action.move(pos_2)
+
 
 @tweezer
 def entangle_move_ltoaom(
@@ -195,13 +138,13 @@ def entangle_move_ltoaom(
     else:
         if start_y[0] < end_y[0]:
             # moving up
-            y_shift = ROW_SEPARATION/2
+            y_shift = ROW_SEPARATION / 2
         else:
             # moving down
-            y_shift = -ROW_SEPARATION/2
+            y_shift = -ROW_SEPARATION / 2
 
         mid_1 = grid.shift(pos_1, 0, y_shift)
-        mid_2 = grid.shift(pos_1, -COL_SEPARATION/2, y_shift)
+        mid_2 = grid.shift(pos_1, -COL_SEPARATION / 2, y_shift)
         mid_3 = grid.shift(pos_2, -GATE_SPACING, 0)
 
         action.set_loc(pos_1)
@@ -210,6 +153,7 @@ def entangle_move_ltoaom(
         action.move(mid_2)
         action.move(mid_3)
         action.move(pos_2)
+
 
 @tweezer
 def entangle_move_rtor(
@@ -235,6 +179,7 @@ def entangle_move_rtor(
     action.move(mid_2)
     action.move(pos_2)
 
+
 @tweezer
 def entangle_move_rtoaom(
     controls: grid.Grid[Any, Any],
@@ -250,8 +195,8 @@ def entangle_move_rtoaom(
     pos_1 = grid.from_positions(start_x, start_y)
     pos_2 = grid.from_positions(end_x, end_y)
 
-    mid_1 = grid.shift(pos_1, 0, ROW_SEPARATION/2)
-    mid_2 = grid.shift(pos_2, 0, ROW_SEPARATION/2)
+    mid_1 = grid.shift(pos_1, 0, ROW_SEPARATION / 2)
+    mid_2 = grid.shift(pos_2, 0, ROW_SEPARATION / 2)
 
     action.set_loc(pos_1)
     action.turn_on(action.ALL, action.ALL)
@@ -259,11 +204,10 @@ def entangle_move_rtoaom(
     action.move(mid_2)
     action.move(pos_2)
 
+
 @move
 def run_interzone_move(
-    storage_grid: grid.Grid[Any, Any],
-    entangling_grid: grid.Grid[Any, Any],
-    left:bool
+    storage_grid: grid.Grid[Any, Any], entangling_grid: grid.Grid[Any, Any], left: bool
 ):
     xtones = ilist.range(len(grid.get_xpos(storage_grid)))
     ytones = ilist.range(len(grid.get_ypos(storage_grid)))
@@ -273,6 +217,7 @@ def run_interzone_move(
 
     dtask(storage_grid, entangling_grid, left)
     rev_dtask(storage_grid, entangling_grid, left)
+
 
 @move
 def run_entangle_move_ltor(
@@ -289,6 +234,7 @@ def run_entangle_move_ltor(
     # apply_cx(gate_zone)
     rev_dtask(controls, targets)
 
+
 @move
 def run_entangle_move_ltoaom(
     controls: grid.Grid[Any, Any],
@@ -303,6 +249,7 @@ def run_entangle_move_ltoaom(
     dtask(controls, targets)
     # apply_cx(gate_zone)
     rev_dtask(controls, targets)
+
 
 @move
 def run_entangle_move_rtor(
@@ -319,6 +266,7 @@ def run_entangle_move_rtor(
     # apply_cx(gate_zone)
     rev_dtask(controls, targets)
 
+
 @move
 def run_entangle_move_rtoaom(
     controls: grid.Grid[Any, Any],
@@ -334,69 +282,96 @@ def run_entangle_move_rtoaom(
     # apply_cx(gate_zone)
     rev_dtask(controls, targets)
 
+
 def generate_moves():
 
     num_x = 17
     num_y = 5
     column_spacing = COL_SEPARATION
     row_spacing = ROW_SEPARATION
-    gate_spacing = GATE_SPACING #space between atoms for CZ gate.
+    gate_spacing = GATE_SPACING  # space between atoms for CZ gate.
 
-    x_spacing = sum(repeat((gate_spacing, column_spacing), num_x - 1), ()) + (gate_spacing,)
+    x_spacing = sum(repeat((gate_spacing, column_spacing), num_x - 1), ()) + (
+        gate_spacing,
+    )
     y_spacing = tuple(repeat(row_spacing, num_y - 1))
-    all_entangling_zone_traps = grid.Grid(x_spacing, y_spacing, -81.0, -20.0) # offset to center the grid such that 0,0 is the middle of the AOD.
+    all_entangling_zone_traps = grid.Grid(
+        x_spacing, y_spacing, -81.0, -20.0
+    )  # offset to center the grid such that 0,0 is the middle of the AOD.
     left_trap_x_indices = ilist.IList(range(0, num_x * 2, 2))
     right_trap_x_indices = ilist.IList(range(1, num_x * 2, 2))
     all_y_indices = ilist.IList(range(num_y))
     left_traps = all_entangling_zone_traps.get_view(left_trap_x_indices, all_y_indices)
-    right_traps = all_entangling_zone_traps.get_view(right_trap_x_indices, all_y_indices)
+    right_traps = all_entangling_zone_traps.get_view(
+        right_trap_x_indices, all_y_indices
+    )
 
-    aom_sites = left_traps.shift(-2,0)
+    aom_sites = left_traps.shift(-2, 0)
 
     reservoir_ally_width = STORAGE_ALLY_WIDTH
-    reservoir_column_spacing = column_spacing+gate_spacing-reservoir_ally_width
+    reservoir_column_spacing = column_spacing + gate_spacing - reservoir_ally_width
     reservoir_row_spacing = 4.0
     num_reservoir_rows = 19
 
-    reservoir_x_spacing = sum(repeat((reservoir_ally_width, reservoir_column_spacing), num_x - 1), ()) + (reservoir_ally_width,)
+    reservoir_x_spacing = sum(
+        repeat((reservoir_ally_width, reservoir_column_spacing), num_x - 1), ()
+    ) + (reservoir_ally_width,)
     reservoir_y_spacing = tuple(repeat(reservoir_row_spacing, num_reservoir_rows - 1))
 
-    top_reservoir = grid.Grid(reservoir_x_spacing, reservoir_y_spacing, -81.0 - reservoir_ally_width, row_spacing * 3)
-    bottom_reservoir = grid.Grid(reservoir_x_spacing, reservoir_y_spacing, -81.0 - reservoir_ally_width, -3.0 * row_spacing - reservoir_row_spacing * (num_reservoir_rows-1))
+    top_reservoir = grid.Grid(
+        reservoir_x_spacing,
+        reservoir_y_spacing,
+        -81.0 - reservoir_ally_width,
+        row_spacing * 3,
+    )
+    bottom_reservoir = grid.Grid(
+        reservoir_x_spacing,
+        reservoir_y_spacing,
+        -81.0 - reservoir_ally_width,
+        -3.0 * row_spacing - reservoir_row_spacing * (num_reservoir_rows - 1),
+    )
 
-    SL0_block_x_indices = ilist.IList(range(4,4+7*2,2))
-    SL0_block_y_indices = ilist.IList(range(8,8+5*2,2))
+    SL0_block_x_indices = ilist.IList(range(4, 4 + 7 * 2, 2))
+    SL0_block_y_indices = ilist.IList(range(8, 8 + 5 * 2, 2))
     SL0_block_traps = top_reservoir.get_view(SL0_block_x_indices, SL0_block_y_indices)
 
     SR0_block_x_indices = ilist.IList(range(5, 5 + 7 * 2, 2))
     SR0_block_y_indices = ilist.IList(range(8, 8 + 5 * 2, 2))
     SR0_block_traps = top_reservoir.get_view(SR0_block_x_indices, SR0_block_y_indices)
 
-    SL1_block_x_indices = ilist.IList(range(4 + 7*2, 4 + 7*2 + 7 * 2, 2))
+    SL1_block_x_indices = ilist.IList(range(4 + 7 * 2, 4 + 7 * 2 + 7 * 2, 2))
     SL1_block_y_indices = ilist.IList(range(8, 8 + 5 * 2, 2))
     SL1_block_traps = top_reservoir.get_view(SL1_block_x_indices, SL1_block_y_indices)
 
-    SR1_block_x_indices = ilist.IList(range(5 + 7*2, 5 + 7*2 + 7 * 2, 2))
+    SR1_block_x_indices = ilist.IList(range(5 + 7 * 2, 5 + 7 * 2 + 7 * 2, 2))
     SR1_block_y_indices = ilist.IList(range(8, 8 + 5 * 2, 2))
     SR1_block_traps = top_reservoir.get_view(SR1_block_x_indices, SR1_block_y_indices)
 
     ML0_block_x_indices = ilist.IList(range(4, 4 + 7 * 2, 2))
     ML0_block_y_indices = ilist.IList(range(2, 2 + 5 * 2, 2))
-    ML0_block_traps = bottom_reservoir.get_view(ML0_block_x_indices, ML0_block_y_indices)
+    ML0_block_traps = bottom_reservoir.get_view(
+        ML0_block_x_indices, ML0_block_y_indices
+    )
 
     MR0_block_x_indices = ilist.IList(range(5, 5 + 7 * 2, 2))
     MR0_block_y_indices = ilist.IList(range(2, 2 + 5 * 2, 2))
-    MR0_block_traps = bottom_reservoir.get_view(MR0_block_x_indices, MR0_block_y_indices)
+    MR0_block_traps = bottom_reservoir.get_view(
+        MR0_block_x_indices, MR0_block_y_indices
+    )
 
     ML1_block_x_indices = ilist.IList(range(4 + 7 * 2, 4 + 7 * 2 + 7 * 2, 2))
     ML1_block_y_indices = ilist.IList(range(2, 2 + 5 * 2, 2))
-    ML1_block_traps = bottom_reservoir.get_view(ML1_block_x_indices, ML1_block_y_indices)
+    ML1_block_traps = bottom_reservoir.get_view(
+        ML1_block_x_indices, ML1_block_y_indices
+    )
 
     MR1_block_x_indices = ilist.IList(range(5 + 7 * 2, 5 + 7 * 2 + 7 * 2, 2))
     MR1_block_y_indices = ilist.IList(range(2, 2 + 5 * 2, 2))
-    MR1_block_traps = bottom_reservoir.get_view(MR1_block_x_indices, MR1_block_y_indices)
+    MR1_block_traps = bottom_reservoir.get_view(
+        MR1_block_x_indices, MR1_block_y_indices
+    )
 
-    G0_block_x_indices = ilist.IList(range(2, 2+7))
+    G0_block_x_indices = ilist.IList(range(2, 2 + 7))
     GL0_block_traps = left_traps.get_view(G0_block_x_indices, all_y_indices)
     GR0_block_traps = right_traps.get_view(G0_block_x_indices, all_y_indices)
 
@@ -428,13 +403,20 @@ def generate_moves():
     logical_cols = 2
 
     home_x_indices = {
-        logical_q_idx: ilist.IList([(q_idx % 14)+2 for q_idx in range(logical_q_idx * 7, logical_q_idx * 7 + 7)]) for
-        logical_q_idx in range(int(logical_cols * logical_rows))
+        logical_q_idx: ilist.IList(
+            [
+                (q_idx % 14) + 2
+                for q_idx in range(logical_q_idx * 7, logical_q_idx * 7 + 7)
+            ]
+        )
+        for logical_q_idx in range(int(logical_cols * logical_rows))
     }
 
     home_y_indices = {
-        logical_q_idx: ilist.IList([q_idx // 14 for q_idx in range(logical_q_idx * 7, logical_q_idx * 7 + 7)]) for
-        logical_q_idx in range(int(num_x * num_y / 7))
+        logical_q_idx: ilist.IList(
+            [q_idx // 14 for q_idx in range(logical_q_idx * 7, logical_q_idx * 7 + 7)]
+        )
+        for logical_q_idx in range(int(num_x * num_y / 7))
     }
 
     def add_zones(control_logicals, target_logicals, move_index):
@@ -456,13 +438,17 @@ def generate_moves():
         target_y_indices = ilist.IList(sorted(set(target_y_indices)))
 
         qubit_moves = {
-            f'controls_{move_index}': left_traps.get_view(control_x_indices, control_y_indices),
-            f'targets_{move_index}': left_traps.get_view(target_x_indices, target_y_indices),
-        } #controls will move to targets
+            f"controls_{move_index}": left_traps.get_view(
+                control_x_indices, control_y_indices
+            ),
+            f"targets_{move_index}": left_traps.get_view(
+                target_x_indices, target_y_indices
+            ),
+        }  # controls will move to targets
 
         static_traps.update(qubit_moves)
 
-    add_zones(control_logicals = [0,2,4,6], target_logicals = [2,4,6,8], move_index=0)
+    add_zones(control_logicals=[0, 2, 4, 6], target_logicals=[2, 4, 6, 8], move_index=0)
     add_zones(control_logicals=[0, 2, 4], target_logicals=[4, 6, 8], move_index=1)
     add_zones(control_logicals=[0, 2], target_logicals=[6, 8], move_index=2)
     add_zones(control_logicals=[0], target_logicals=[8], move_index=3)
@@ -471,37 +457,67 @@ def generate_moves():
     add_zones(control_logicals=[8, 6, 4], target_logicals=[4, 2, 0], move_index=6)
     add_zones(control_logicals=[8, 6, 4, 2], target_logicals=[6, 4, 2, 0], move_index=7)
 
-    add_zones(control_logicals = [1,3,5,7], target_logicals = [3,5,7,9], move_index=8)
+    add_zones(control_logicals=[1, 3, 5, 7], target_logicals=[3, 5, 7, 9], move_index=8)
     add_zones(control_logicals=[1, 3, 5], target_logicals=[5, 7, 9], move_index=9)
     add_zones(control_logicals=[1, 3], target_logicals=[7, 9], move_index=10)
     add_zones(control_logicals=[1], target_logicals=[9], move_index=11)
     add_zones(control_logicals=[9], target_logicals=[1], move_index=12)
     add_zones(control_logicals=[9, 7], target_logicals=[3, 1], move_index=13)
     add_zones(control_logicals=[9, 7, 5], target_logicals=[5, 3, 1], move_index=14)
-    add_zones(control_logicals=[9, 7, 5, 3], target_logicals=[7, 5, 3, 1], move_index=15)
+    add_zones(
+        control_logicals=[9, 7, 5, 3], target_logicals=[7, 5, 3, 1], move_index=15
+    )
 
-    add_zones(control_logicals=[0,2,4,6,8], target_logicals=[1,3,5,7,9], move_index=16)
+    add_zones(
+        control_logicals=[0, 2, 4, 6, 8], target_logicals=[1, 3, 5, 7, 9], move_index=16
+    )
 
-    add_zones(control_logicals=[0,2,4,6,8], target_logicals=[0,2,4,6,8], move_index=17)
-    add_zones(control_logicals=[1,3,5,7,9], target_logicals=[1,3,5,7,9], move_index=18)
+    add_zones(
+        control_logicals=[0, 2, 4, 6, 8], target_logicals=[0, 2, 4, 6, 8], move_index=17
+    )
+    add_zones(
+        control_logicals=[1, 3, 5, 7, 9], target_logicals=[1, 3, 5, 7, 9], move_index=18
+    )
 
     layout = spec.Layout(
         static_traps=static_traps,
-        fillable=set(["GL0_block","GL1_block","SL0_block","SR0_block","SL1_block","SR1_block", "ML0_block","MR0_block","ML1_block","MR1_block"]),
+        fillable=set(
+            [
+                "GL0_block",
+                "GL1_block",
+                "SL0_block",
+                "SR0_block",
+                "SL1_block",
+                "SR1_block",
+                "ML0_block",
+                "MR0_block",
+                "ML1_block",
+                "MR1_block",
+            ]
+        ),
         has_cz=set(["traps"]),
         has_local=set(["left_traps"]),
-        special_grid ={"aom_sites": aom_sites}
+        special_grid={"aom_sites": aom_sites},
     )
 
     # layout.show()
 
-    spec_value = spec.ArchSpec(
-        layout=layout
-    )
+    spec_value = spec.ArchSpec(layout=layout)
 
     @move
     def main():
-        init.fill([spec.get_static_trap(zone_id="SL0_block"), spec.get_static_trap(zone_id="SR0_block"),spec.get_static_trap(zone_id="SL1_block"), spec.get_static_trap(zone_id="SR1_block"), spec.get_static_trap(zone_id="ML0_block"), spec.get_static_trap(zone_id="MR0_block"),spec.get_static_trap(zone_id="ML1_block"), spec.get_static_trap(zone_id="MR1_block")])
+        init.fill(
+            [
+                spec.get_static_trap(zone_id="SL0_block"),
+                spec.get_static_trap(zone_id="SR0_block"),
+                spec.get_static_trap(zone_id="SL1_block"),
+                spec.get_static_trap(zone_id="SR1_block"),
+                spec.get_static_trap(zone_id="ML0_block"),
+                spec.get_static_trap(zone_id="MR0_block"),
+                spec.get_static_trap(zone_id="ML1_block"),
+                spec.get_static_trap(zone_id="MR1_block"),
+            ]
+        )
         # Add some movement operations here
 
         controls = spec.get_static_trap(zone_id="controls_0")
@@ -712,8 +728,8 @@ def generate_moves():
         targets = spec.get_static_trap(zone_id="targets_16")
         run_entangle_move_rtoaom(controls, targets)
 
-
     return main, spec_value
+
 
 def run_plotter():
     main, spec_value = generate_moves()
