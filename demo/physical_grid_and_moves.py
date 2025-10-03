1a256af13412dbb050763a734d2b77c32b23c32b
Phillip Weinberg <weinbe58@gmail.com>
2025-10-03 11:25:04 -0400

    making linter happy with new version of bloqade-geometry

--- demo/physical_grid_and_moves.py
+++ demo/physical_grid_and_moves.py
@@ -1,25 +1,17 @@
-from itertools import repeat, chain
-from typing import Any, Literal, TypeVar, List
-
+from itertools import chain, repeat
+from typing import Any, TypeVar
 
 from bloqade.geometry.dialects import grid
+from bloqade.insight import kernel
+from bloqade.insight.interp import AnimationInterpreter
+from bloqade.insight.legacy.animate import FieldOfView, animate
+from bloqade.insight.upstream.shuttle import ShuttleToInsight
+from kirin import ir
 from kirin.dialects import ilist
 
-from bloqade.shuttle import action, gate, init, measure, schedule, spec
-
+from bloqade.shuttle import action, init, schedule, spec
 from bloqade.shuttle.prelude import move, tweezer
-
-from bloqade.shuttle.stdlib.layouts.asserts import assert_sorted
-
 from bloqade.shuttle.visualizer import MatplotlibRenderer, PathVisualizer
-
-from bloqade.insight.upstream.shuttle import ShuttleToInsight
-from bloqade.insight.interp import AnimationInterpreter
-from bloqade.insight.legacy.animate import animate, FieldOfView
-from bloqade.insight import kernel
-from kirin import ir
-
-import matplotlib.pyplot as plt
 
 
 def animate_shuttle(
@@ -75,9 +67,11 @@ def animate_shuttle(
         n_blocks=n_blocks,
     )
 
+
 # Define type variables for generic programming
-NumX = TypeVar('NumX', bound=int)
-NumY = TypeVar('NumY', bound=int)
+NumX = TypeVar("NumX", bound=int)
+NumY = TypeVar("NumY", bound=int)
+
 
 @tweezer
 def row_move_impl(
@@ -86,13 +80,11 @@ def row_move_impl(
 ):
 
     start_x = grid.get_xpos(controls)
-    start_y = grid.get_ypos(controls)
 
     end_x = grid.get_xpos(targets)
-    end_y = grid.get_ypos(targets)
 
     mid_1 = grid.shift(controls, 0, 5)
-    mid_2 = grid.shift(controls, 4 + (end_x[0]-start_x[0]), 5)
+    mid_2 = grid.shift(controls, 4 + (end_x[0] - start_x[0]), 5)
     mid_3 = grid.shift(targets, 4, 0)
 
     action.set_loc(controls)
@@ -101,6 +93,7 @@ def row_move_impl(
     action.move(mid_2)
     action.move(mid_3)
     action.move(targets)
+
 
 @move
 def run_move(
@@ -113,8 +106,9 @@ def run_move(
     dtask = schedule.device_fn(row_move_impl, xtones, ytones)
     rev_dtask = schedule.reverse(dtask)
 
-    dtask(controls,targets)
+    dtask(controls, targets)
     rev_dtask(controls, targets)
+
 
 def generate_moves():
 
@@ -122,69 +116,87 @@ def generate_moves():
     num_y = 5
     column_spacing = 8.0
     row_spacing = 10.0
-    gate_spacing = 2.0 #space between atoms for CZ gate.
+    gate_spacing = 2.0  # space between atoms for CZ gate.
 
-    x_spacing = sum(repeat((gate_spacing, column_spacing), num_x - 1), ()) + (gate_spacing,)
+    x_spacing = sum(repeat((gate_spacing, column_spacing), num_x - 1), ()) + (
+        gate_spacing,
+    )
     y_spacing = tuple(repeat(row_spacing, num_y - 1))
-    all_entangling_zone_traps = grid.Grid(x_spacing, y_spacing, -81.0, -20.0) # offset to center the grid such that 0,0 is the middle of the AOD.
+    all_entangling_zone_traps = grid.Grid(
+        x_spacing, y_spacing, -81.0, -20.0
+    )  # offset to center the grid such that 0,0 is the middle of the AOD.
     left_trap_x_indices = ilist.IList(range(0, num_x * 2, 2))
     right_trap_x_indices = ilist.IList(range(1, num_x * 2, 2))
     all_y_indices = ilist.IList(range(num_y))
     left_traps = all_entangling_zone_traps.get_view(left_trap_x_indices, all_y_indices)
-    right_traps = all_entangling_zone_traps.get_view(right_trap_x_indices, all_y_indices)
+    right_traps = all_entangling_zone_traps.get_view(
+        right_trap_x_indices, all_y_indices
+    )
 
-    aom_sites = left_traps.shift(-2,0)
+    aom_sites = left_traps.shift(-2, 0)
 
     reservoir_ally_width = 6.0
-    reservoir_column_spacing = column_spacing+gate_spacing-reservoir_ally_width
+    reservoir_column_spacing = column_spacing + gate_spacing - reservoir_ally_width
     reservoir_row_spacing = 4.0
     num_reservoir_rows = 19
 
-    reservoir_x_spacing = sum(repeat((reservoir_ally_width, reservoir_column_spacing), num_x - 1), ()) + (reservoir_ally_width,)
+    reservoir_x_spacing = sum(
+        repeat((reservoir_ally_width, reservoir_column_spacing), num_x - 1), ()
+    ) + (reservoir_ally_width,)
     reservoir_y_spacing = tuple(repeat(reservoir_row_spacing, num_reservoir_rows - 1))
 
-    top_reservoir = grid.Grid(reservoir_x_spacing, reservoir_y_spacing, -81.0 - reservoir_ally_width, row_spacing * 3)
-    bottom_reservoir = grid.Grid(reservoir_x_spacing, reservoir_y_spacing, -81.0 - reservoir_ally_width, -3.0 * row_spacing - reservoir_row_spacing * (num_reservoir_rows-1))
+    top_reservoir = grid.Grid(
+        reservoir_x_spacing,
+        reservoir_y_spacing,
+        -81.0 - reservoir_ally_width,
+        row_spacing * 3,
+    )
+    bottom_reservoir = grid.Grid(
+        reservoir_x_spacing,
+        reservoir_y_spacing,
+        -81.0 - reservoir_ally_width,
+        -3.0 * row_spacing - reservoir_row_spacing * (num_reservoir_rows - 1),
+    )
 
-    SL_block_x_indices = ilist.IList(range(2,2+8*2,2))
-    SL_block_y_indices = ilist.IList(range(8,8+4*2,2))
+    SL_block_x_indices = ilist.IList(range(2, 2 + 8 * 2, 2))
+    SL_block_y_indices = ilist.IList(range(8, 8 + 4 * 2, 2))
     SL_block_traps = top_reservoir.get_view(SL_block_x_indices, SL_block_y_indices)
 
-    SR_block_x_indices = ilist.IList(range(3,3+8*2,2))
-    SR_block_y_indices = ilist.IList(range(8,8+4*2,2))
+    SR_block_x_indices = ilist.IList(range(3, 3 + 8 * 2, 2))
+    SR_block_y_indices = ilist.IList(range(8, 8 + 4 * 2, 2))
     SR_block_traps = top_reservoir.get_view(SR_block_x_indices, SR_block_y_indices)
 
-    ML_block_x_indices = ilist.IList(range(2,2+8*2,2))
-    ML_block_y_indices = ilist.IList(range(2,2+4*2,2))
+    ML_block_x_indices = ilist.IList(range(2, 2 + 8 * 2, 2))
+    ML_block_y_indices = ilist.IList(range(2, 2 + 4 * 2, 2))
     ML_block_traps = top_reservoir.get_view(ML_block_x_indices, ML_block_y_indices)
 
-    MR_block_x_indices = ilist.IList(range(3,3+8*2,2))
-    MR_block_y_indices = ilist.IList(range(2,2+4*2,2))
+    MR_block_x_indices = ilist.IList(range(3, 3 + 8 * 2, 2))
+    MR_block_y_indices = ilist.IList(range(2, 2 + 4 * 2, 2))
     MR_block_traps = top_reservoir.get_view(MR_block_x_indices, MR_block_y_indices)
 
-    G_block_x_indices  = ilist.IList(range(1,1+16))
+    G_block_x_indices = ilist.IList(range(1, 1 + 16))
     G_block_y_indices = all_y_indices[:-1]
     GL_block_traps = left_traps.get_view(G_block_x_indices, G_block_y_indices)
     GR_block_traps = right_traps.get_view(G_block_x_indices, G_block_y_indices)
 
-    evenL_rows = GL_block_traps.get_view(list(range(0,16)), [0,2])
+    evenL_rows = GL_block_traps.get_view(list(range(0, 16)), [0, 2])
 
-    oddR_rows = GR_block_traps.get_view(list(range(0,16)), [1,3])
+    oddR_rows = GR_block_traps.get_view(list(range(0, 16)), [1, 3])
 
-    evenL_cols = GL_block_traps.get_view(range(0,16,2), range(4))
-    oddR_cols = GR_block_traps.get_view(range(1,16,2), range(4))
+    evenL_cols = GL_block_traps.get_view(range(0, 16, 2), range(4))
+    oddR_cols = GR_block_traps.get_view(range(1, 16, 2), range(4))
 
-    first2L_rows =GL_block_traps.get_view(range(16), [0, 1])
+    first2L_rows = GL_block_traps.get_view(range(16), [0, 1])
     second2R_rows = GR_block_traps.get_view(range(16), [2, 3])
 
-    first2L_cols = GL_block_traps.get_view([0,1,4,5,8,9,12,13], range(4))
-    second2R_cols = GR_block_traps.get_view([2,3,6,7,10,11,14,15], range(4))
+    first2L_cols = GL_block_traps.get_view([0, 1, 4, 5, 8, 9, 12, 13], range(4))
+    second2R_cols = GR_block_traps.get_view([2, 3, 6, 7, 10, 11, 14, 15], range(4))
 
-    fourL_block_02 = GL_block_traps.get_view([0,1,2,3,8,9,10,11], range(4))
-    fourR_block_13 = GR_block_traps.get_view([4,5,6,7,12,13,14,15], range(4))
+    fourL_block_02 = GL_block_traps.get_view([0, 1, 2, 3, 8, 9, 10, 11], range(4))
+    fourR_block_13 = GR_block_traps.get_view([4, 5, 6, 7, 12, 13, 14, 15], range(4))
 
     fourL_block_01 = GL_block_traps.get_view(range(8), range(4))
-    fourR_block_23 = GR_block_traps.get_view(range(8,16), range(4))
+    fourR_block_23 = GR_block_traps.get_view(range(8, 16), range(4))
 
     secondL_row = GL_block_traps.get_view(range(16), [1])
     thirdR_row = GR_block_traps.get_view(range(16), [2])
@@ -192,18 +204,17 @@ def generate_moves():
     fourthL_row = GL_block_traps.get_view(range(16), [3])
     firstR_row = GR_block_traps.get_view(range(16), [0])
 
-    secondL_cols = GL_block_traps.get_view(range(1,16,4), range(4))
-    thirdR_cols = GR_block_traps.get_view(range(2,16,4), range(4))
+    secondL_cols = GL_block_traps.get_view(range(1, 16, 4), range(4))
+    thirdR_cols = GR_block_traps.get_view(range(2, 16, 4), range(4))
 
-    fourthL_cols = GL_block_traps.get_view(range(3,16,4), range(4))
-    firstR_cols = GR_block_traps.get_view(range(0,16,4), range(4))
+    fourthL_cols = GL_block_traps.get_view(range(3, 16, 4), range(4))
+    firstR_cols = GR_block_traps.get_view(range(0, 16, 4), range(4))
 
-    fourL_block_1 = GL_block_traps.get_view(range(4,8), range(4))
-    fourR_block_2 = GR_block_traps.get_view(range(8,12), range(4))
+    fourL_block_1 = GL_block_traps.get_view(range(4, 8), range(4))
+    fourR_block_2 = GR_block_traps.get_view(range(8, 12), range(4))
 
-    fourL_block_3 = GL_block_traps.get_view(range(12,16), range(4))
+    fourL_block_3 = GL_block_traps.get_view(range(12, 16), range(4))
     fourR_block_0 = GR_block_traps.get_view(range(4), range(4))
-
 
     static_traps = {
         "traps": all_entangling_zone_traps,
@@ -227,42 +238,53 @@ def generate_moves():
         "second2R_cols": second2R_cols,
         "fourL_block_02": fourL_block_02,
         "fourR_block_13": fourR_block_13,
-        'fourL_block_01': fourL_block_01,
-        'fourR_block_23': fourR_block_23,
-        'secondL_row': secondL_row,
-        'thirdR_row': thirdR_row,
-        'fourthL_row': fourthL_row,
-        'firstR_row': firstR_row,
-        'secondL_cols': secondL_cols,
-        'thirdR_cols': thirdR_cols,
-        'fourthL_cols': fourthL_cols,
-        'firstR_clos': firstR_cols,
-        'fourL_block_1': fourL_block_1,
-        'fourR_block_2': fourR_block_2,
-        'fourL_block_3': fourL_block_3,
-        'fourR_block_0': fourR_block_0,
+        "fourL_block_01": fourL_block_01,
+        "fourR_block_23": fourR_block_23,
+        "secondL_row": secondL_row,
+        "thirdR_row": thirdR_row,
+        "fourthL_row": fourthL_row,
+        "firstR_row": firstR_row,
+        "secondL_cols": secondL_cols,
+        "thirdR_cols": thirdR_cols,
+        "fourthL_cols": fourthL_cols,
+        "firstR_clos": firstR_cols,
+        "fourL_block_1": fourL_block_1,
+        "fourR_block_2": fourR_block_2,
+        "fourL_block_3": fourL_block_3,
+        "fourR_block_0": fourR_block_0,
     }
 
     layout = spec.Layout(
         static_traps=static_traps,
-        fillable=set(["GL0_block","GL1_block","SL0_block","SR0_block","SL1_block","SR1_block", "ML0_block","MR0_block","ML1_block","MR1_block"]),
+        fillable=set(
+            [
+                "GL0_block",
+                "GL1_block",
+                "SL0_block",
+                "SR0_block",
+                "SL1_block",
+                "SR1_block",
+                "ML0_block",
+                "MR0_block",
+                "ML1_block",
+                "MR1_block",
+            ]
+        ),
         has_cz=set(["traps"]),
         has_local=set(["left_traps"]),
-        special_grid ={"aom_sites": aom_sites}
+        special_grid={"aom_sites": aom_sites},
     )
 
     # layout.show()
 
-    spec_value = spec.ArchSpec(
-        layout=layout
-    )
+    spec_value = spec.ArchSpec(layout=layout)
 
     @move
     def main():
         init.fill([spec.get_static_trap(zone_id="left_traps")])
         # Add some movement operations here
 
-        #Start with hypercube moves
+        # Start with hypercube moves
 
         controls = spec.get_static_trap(zone_id="evenL_rows")
         targets = spec.get_static_trap(zone_id="oddR_rows")
@@ -316,8 +338,8 @@ def generate_moves():
 
         # Storage zone moves undone.
 
-
     return main, spec_value
+
 
 def run_plotter():
     main, spec_value = generate_moves()
